26bf4163278a28c723d393685bc7c7c219bdb9e7
David Denton <denton.david@gmail.com>
2020-09-12 18:01:40 +0100

    run in docker

--- quarkus/build_and_run.sh
+++ quarkus/build_and_run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Building http4k for Quarkus..."
-
 ./gradlew clean test buildNative
 
-echo "Running http4k on Quarkus..."
+echo "Adding app to Docker image..."
+docker build . -t http4k-quarkus-example
 
+echo "Running http4k on Quarkus..."
 docker run -p 8080:8080 http4k-quarkus-example
