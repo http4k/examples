4e66e20472e3be5ec11094a8bd5ba56b5876bc7f
Ivan Sanchez <ivan@gourame.com>
2020-09-17 18:13:55 +0100

    Allow lambda logging to cloudwatch

--- aws-lambda/scripts/ensure_role_exists.sh
+++ aws-lambda/scripts/ensure_role_exists.sh
@@ -6,6 +6,7 @@ ROLES=$(aws iam list-roles | jq  '.Roles | .[] | select(.RoleName =="lambda-ex")
 if [ -z "${ROLES}" ] ; then
   echo "Creating lambda execution role..."
   aws iam create-role --role-name lambda-ex --assume-role-policy-document file://$DIR/trust-policy.json 1>/dev/null
+  aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole --role-name lambda-ex
   sleep 10
 else
   echo "Role already exists. Skipping..."
