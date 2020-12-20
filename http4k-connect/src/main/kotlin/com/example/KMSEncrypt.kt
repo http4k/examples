20cfb3852cd722a6439239027d94c8c1b9a11f41
David Denton <denton.david@gmail.com>
2020-12-20 20:10:15 +0000

    add KMS example server

---
+++ http4k-connect/src/main/kotlin/com/example/KMSEncrypt.kt
@@ -0,0 +1,39 @@
+package com.example
+
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Success
+import org.http4k.aws.AwsCredentialScope
+import org.http4k.aws.AwsCredentials
+import org.http4k.client.JavaHttpClient
+import org.http4k.connect.amazon.kms.Http
+import org.http4k.connect.amazon.kms.KMS
+import org.http4k.connect.amazon.kms.encrypt
+import org.http4k.connect.amazon.model.Base64Blob
+import org.http4k.connect.amazon.model.EncryptionAlgorithm.SYMMETRIC_DEFAULT
+import org.http4k.connect.amazon.model.KMSKeyId
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
+fun KMSEncrypt(keyId: KMSKeyId, kmsHttp: HttpHandler): HttpHandler {
+    val kms = KMS.Http(
+        AwsCredentialScope("us-east-1", "kms"), { AwsCredentials("accesskey", "secret") },
+        kmsHttp
+    )
+    return routes("/{value}" bind GET to {
+        when (val result = kms.encrypt(keyId, Base64Blob.encoded(it.path("value")!!), SYMMETRIC_DEFAULT)) {
+            is Success -> Response(OK).body(result.value.CiphertextBlob.decoded())
+            is Failure -> Response(result.reason.status)
+        }
+    })
+}
+
+fun main() {
+    KMSEncrypt(KMSKeyId.of("kmsKeyId"), JavaHttpClient()).asServer(SunHttp(8080)).start()
+}
--- http4k-connect/src/test/kotlin/environment/RunnableEnvironment.kt
+++ http4k-connect/src/test/kotlin/environment/RunnableEnvironment.kt
@@ -1,11 +1,19 @@
 package environment
 
+import com.example.KMSEncrypt
 import com.example.S3BucketContents
+import dev.forkhandles.result4k.valueOrNull
 import org.http4k.aws.AwsCredentialScope
 import org.http4k.aws.AwsCredentials
 import org.http4k.client.JavaHttpClient
+import org.http4k.connect.amazon.kms.FakeKMS
+import org.http4k.connect.amazon.kms.Http
+import org.http4k.connect.amazon.kms.KMS
+import org.http4k.connect.amazon.kms.createKey
 import org.http4k.connect.amazon.model.BucketKey
 import org.http4k.connect.amazon.model.BucketName
+import org.http4k.connect.amazon.model.CustomerMasterKeySpec.SYMMETRIC_DEFAULT
+import org.http4k.connect.amazon.model.KMSKeyId
 import org.http4k.connect.amazon.model.Region
 import org.http4k.connect.amazon.s3.FakeS3
 import org.http4k.connect.amazon.s3.Http
@@ -21,23 +29,26 @@ import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 fun main() {
+    val region = Region.of("us-east-1")
+
     val fakeS3 = FakeS3()
-    val server = fakeS3.start()
-
-    val s3Http = createClientPointedAtLocalFakeS3(server)
-
-    createS3BucketAndFiles(s3Http)
-
+    val s3Server = fakeS3.start()
+    val s3Http = createClientPointedAtLocalFake(s3Server)
+    createS3BucketAndFiles(s3Http, region)
     S3BucketContents(s3Http).asServer(SunHttp(8080)).start()
+
+    val fakeKms = FakeKMS()
+    val kmsServer = fakeKms.start()
+    val kmsHttp = createClientPointedAtLocalFake(kmsServer)
+    val keyId = createKMSKey(kmsHttp, region)
+    KMSEncrypt(keyId, kmsHttp).asServer(SunHttp(9090)).start()
 }
 
-private fun createClientPointedAtLocalFakeS3(server: Http4kServer) =
+private fun createClientPointedAtLocalFake(server: Http4kServer) =
     ClientFilters.SetBaseUriFrom(Uri.of("http://localhost:${server.port()}"))
         .then(JavaHttpClient())
 
-private fun createS3BucketAndFiles(s3Http: HttpHandler) {
-    val region = Region.of("us-east-1")
-
+private fun createS3BucketAndFiles(s3Http: HttpHandler, region: Region) {
     val s3 = S3.Http(
         AwsCredentialScope(region.value, "s3"), { AwsCredentials("accesskey", "secret") },
         s3Http
@@ -54,4 +65,13 @@ private fun createS3BucketAndFiles(s3Http: HttpHandler) {
 
     s3Bucket[BucketKey.of("file1")] = "hello".byteInputStream()
     s3Bucket[BucketKey.of("file2")] = "there".byteInputStream()
+}
+
+private fun createKMSKey(kmsHttp: HttpHandler, region: Region): KMSKeyId {
+    val kms = KMS.Http(
+        AwsCredentialScope(region.value, "kms"), { AwsCredentials("accesskey", "secret") },
+        kmsHttp
+    )
+
+    return kms.createKey(SYMMETRIC_DEFAULT).valueOrNull()!!.KeyMetadata.KeyId
 }
