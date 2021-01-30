704fd4d17651eb6f775082c635f03384a37d63eb
David Denton <denton.david@gmail.com>
2021-01-30 09:13:58 +0000

    tidy imports

--- hotwire/src/main/kotlin/com/example/endpoints.kt
+++ hotwire/src/main/kotlin/com/example/endpoints.kt
@@ -1,8 +1,9 @@
 package com.example
 
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
 import org.http4k.lens.Query
 import org.http4k.lens.boolean
@@ -13,18 +14,18 @@ import org.http4k.websocket.Websocket
 import java.time.LocalDateTime
 
 fun index(lenses: SelectingViewModelLenses) =
-    "/" bind Method.GET to { Response(Status.OK).with(lenses(it) of Index) }
+    "/" bind GET to { Response(OK).with(lenses(it) of Index) }
 
 fun staticContent(hotReload: Boolean) =
     static(if (hotReload) ResourceLoader.Directory("./src/main/resources") else ResourceLoader.Classpath("public"))
 
-fun clicks(lenses: SelectingViewModelLenses) = "/clicks" bind Method.POST to {
-    Response(Status.OK).with(lenses(it) of Clicks(LocalDateTime.now()))
+fun clicks(lenses: SelectingViewModelLenses) = "/clicks" bind POST to {
+    Response(OK).with(lenses(it) of Clicks(LocalDateTime.now()))
 }
 
-fun hello(lenses: SelectingViewModelLenses) = "/hello" bind Method.GET to {
+fun hello(lenses: SelectingViewModelLenses) = "/hello" bind GET to {
     if (Query.boolean().defaulted("sleep", false)(it)) Thread.sleep(1000)
-    Response(Status.OK).with(lenses.htmlViews of HelloWorld(Query.defaulted("person", "world")(it)))
+    Response(OK).with(lenses.htmlViews of HelloWorld(Query.defaulted("person", "world")(it)))
 }
 
 fun time(lenses: SelectingViewModelLenses) = "/time" bind { ws: Websocket ->
