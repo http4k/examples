dd93946868c15261cb5191f36d1aa99e8a8839fe
David Denton <denton.david@gmail.com>
2021-01-30 09:13:21 +0000

    removal of duplicate code which now exists in http4k

--- hotwire/src/main/kotlin/com/example/SelectingViewModelLenses.kt
+++ hotwire/src/main/kotlin/com/example/SelectingViewModelLenses.kt
@@ -1,12 +1,10 @@
 package com.example
 
-import org.http4k.core.ACCEPT
 import org.http4k.core.Body
 import org.http4k.core.ContentType
 import org.http4k.core.ContentType.Companion.TEXT_HTML
 import org.http4k.core.Request
-import org.http4k.core.viewModel
-import org.http4k.lens.Header
+import org.http4k.lens.Header.ACCEPT
 import org.http4k.template.HandlebarsTemplates
 import org.http4k.template.TemplateRenderer
 import org.http4k.template.viewModel
@@ -15,8 +13,9 @@ import org.http4k.websocket.WsMessage
 class SelectingViewModelLenses(
     rendererFn: HandlebarsTemplates.() -> TemplateRenderer
 ) {
-    private val htmlRenderer = rendererFn(HandlebarsTemplates { it
-        .apply { loader.suffix = ".html" } })
+    private val htmlRenderer = rendererFn(HandlebarsTemplates {
+        it.apply { loader.suffix = ".html" }
+    })
     private val turboRenderer = rendererFn(HandlebarsTemplates { it.apply { loader.suffix = ".turbo-stream.html" } })
 
     val websocketViews = WsMessage.viewModel(turboRenderer).toLens()
@@ -28,7 +27,7 @@ class SelectingViewModelLenses(
      */
     operator fun invoke(request: Request) =
         when {
-            Header.ACCEPT(request)?.accepts(ContentType.TURBO_STREAM) == true -> turboViews
+            ACCEPT(request)?.accepts(ContentType.TURBO_STREAM) == true -> turboViews
             else -> htmlViews
         }
 }
