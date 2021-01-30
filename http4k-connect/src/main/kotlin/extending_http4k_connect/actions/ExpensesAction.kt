2bd8db24f33050c0b41bc254af73e9ecdb66ce6d
David Denton <denton.david@gmail.com>
2021-01-30 06:10:30 +0000

    upgrade http4k, connect

--- http4k-connect/src/main/kotlin/extending_http4k_connect/actions/ExpensesAction.kt
+++ http4k-connect/src/main/kotlin/extending_http4k_connect/actions/ExpensesAction.kt
@@ -1,9 +1,11 @@
 package extending_http4k_connect.actions
 
+import dev.forkhandles.result4k.Result
 import org.http4k.connect.Action
+import org.http4k.connect.RemoteFailure
 
 /**
  * This is the interface which will encapsulate all Actions
  * for the ExpensesSystem
  */
-interface ExpensesAction<T> : Action<T>
+interface ExpensesAction<R> : Action<Result<R, RemoteFailure>>
