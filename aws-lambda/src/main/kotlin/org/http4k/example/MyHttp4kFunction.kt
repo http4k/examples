7de3bc5d49f9a3540bb8d3ae8997cb6356ff592f
David Denton <denton.david@gmail.com>
2021-05-25 20:45:47 +0100

    testing function

--- aws-lambda/src/main/kotlin/org/http4k/example/MyHttp4kFunction.kt
+++ aws-lambda/src/main/kotlin/org/http4k/example/MyHttp4kFunction.kt
@@ -7,13 +7,17 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
-import org.http4k.serverless.ApiGatewayV2LambdaFunction
+import org.http4k.serverless.ApiGatewayV1LambdaFunction
+
+val http4kApp = routes(
+    "/echo/{message:.*}" bind GET to {
+        Response(OK).body(
+            it.path("message") ?: "(nothing to echo, use /echo/<message>)"
+        )
+    },
+    "/tea" bind GET to { Response(I_M_A_TEAPOT) },
+    "/" bind GET to { Response(OK).body("ok") }
+)
 
 @Suppress("unused")
-class MyHttp4kFunction : ApiGatewayV2LambdaFunction(
-    routes(
-        "/echo/{message:.*}" bind GET to { Response(OK).body(it.path("message") ?: "(nothing to echo, use /echo/<message>)") },
-        "/tea" bind GET to { Response(I_M_A_TEAPOT) },
-        "/" bind GET to { Response(OK).body("ok") }
-    )
-)
+class MyHttp4kFunction : ApiGatewayV1LambdaFunction(http4kApp)
