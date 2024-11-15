a3562a9872ea975bfea405cdfe624bd03949461b
David Denton <denton.david@gmail.com>
2024-11-15 21:54:11 +0000

    Fix up last example, upgrade gradle

--- http4k-core/http4k-connect/src/main/kotlin/extending_http4k_connect/ExpensesSystem.kt
+++ http4k-core/http4k-connect/src/main/kotlin/extending_http4k_connect/ExpensesSystem.kt
@@ -3,13 +3,14 @@ package extending_http4k_connect
 import dev.forkhandles.result4k.Result
 import extending_http4k_connect.actions.ExpensesAction
 import org.http4k.connect.Http4kConnectAdapter
+import org.http4k.connect.Http4kConnectApiClient
 import org.http4k.connect.RemoteFailure
 
 /**
  * The main system interface. The Annotation is used by Kapt
  * to generate the friendly extension functions.
  */
-@Http4kConnectAdapter
+@Http4kConnectApiClient
 interface ExpensesSystem {
     operator fun <R : Any> invoke(action: ExpensesAction<R>): Result<R, RemoteFailure>
 
