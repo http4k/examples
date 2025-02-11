dfb840d62899b356923d12a2073673d07a421d84
Ivan Sanchez <ivan@gourame.com>
2025-02-11 10:25:17 +0000

    Use http4k plugin

--- http4k-core/migration-ktor/build.gradle.kts
+++ http4k-core/migration-ktor/build.gradle.kts
@@ -15,6 +15,7 @@ application {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
@@ -24,6 +25,11 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation("org.http4k:http4k-bridge-ktor:${properties["http4kVersion"]}")
+//    implementation("io.ktor:ktor-server-core-jvm:3.0.3")
+//    implementation("io.ktor:ktor-server-host-common-jvm:3.0.3")
+//    implementation("io.ktor:ktor-server-status-pages-jvm:3.0.3")
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- http4k-core/migration-ktor/src/main/kotlin/Application.kt
+++ http4k-core/migration-ktor/src/main/kotlin/Application.kt
@@ -1,11 +1,23 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.http4k.bridge.KtorToHttp4kApplicationPlugin
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
-    configureRouting()
+    install(KtorToHttp4kApplicationPlugin { Response(OK).body("Hello from http4k") })
+
+    routing {
+        get("/") {
+            call.respondText("Hello From Ktor!")
+        }
+    }
 }
