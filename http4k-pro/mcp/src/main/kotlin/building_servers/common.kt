19b1730441c9e6698a39c9d1104ebb0c1970e3af
David Denton <denton.david@gmail.com>
2025-05-06 20:37:36 +0100

    Fix example

--- http4k-pro/mcp/src/main/kotlin/building_servers/common.kt
+++ http4k-pro/mcp/src/main/kotlin/building_servers/common.kt
@@ -4,6 +4,7 @@ import org.http4k.client.JavaHttpClient
 import org.http4k.connect.model.Role
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
+import org.http4k.core.Request
 import org.http4k.core.Uri
 import org.http4k.lens.int
 import org.http4k.mcp.CompletionResponse
@@ -20,6 +21,8 @@ import org.http4k.mcp.model.Resource
 import org.http4k.mcp.model.ResourceName
 import org.http4k.mcp.model.ResourceUriTemplate
 import org.http4k.mcp.model.Tool
+import org.http4k.mcp.model.int
+import org.http4k.mcp.model.string
 import org.http4k.mcp.server.capability.PromptCapability
 import org.http4k.mcp.server.capability.ToolCapability
 import org.http4k.routing.bind
@@ -27,7 +30,7 @@ import org.http4k.routing.compose
 import org.jsoup.Jsoup
 
 fun LinksOnPage(http: HttpHandler): ResourceHandler = {
-    val htmlPage = http(org.http4k.core.Request(GET, it.uri))
+    val htmlPage = http(Request(GET, it.uri))
 
     val links = Jsoup.parse(htmlPage.bodyString())
         .allElements.toList()
@@ -73,7 +76,7 @@ fun countingTool(): ToolCapability {
 }
 
 fun reverseTool(): ToolCapability {
-    val input = Tool.Arg.required("name")
+    val input = Tool.Arg.string().required("name")
 
     return Tool("reverse", "description", input) bind {
         ToolResponse.Ok(listOf(Content.Text(input(it).reversed())))
@@ -81,7 +84,7 @@ fun reverseTool(): ToolCapability {
 }
 
 fun liveWeatherTool(): ToolCapability {
-    val input = Tool.Arg.required("city")
+    val input = Tool.Arg.string().required("city")
 
     return Tool(
         "weather",
