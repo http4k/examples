301ff226be2a0674b6cb4baf38c9beb67ccedc2d
David Denton <denton.david@gmail.com>
2025-06-02 17:22:48 +0100

    Fix example for graphql

--- http4k-core/hello-world/src/main/java/dev/langchain4j/memory/ChatMemory.kt
+++ http4k-core/hello-world/src/main/java/dev/langchain4j/memory/ChatMemory.kt
@@ -1,41 +1,38 @@
-package dev.langchain4j.memory;
+package dev.langchain4j.memory
 
-import dev.langchain4j.data.message.ChatMessage;
-
-import java.util.List;
+import dev.langchain4j.data.message.ChatMessage
 
 /**
  * Represents the memory (history) of a chat conversation.
  * Since language models do not keep the state of the conversation, it is necessary to provide all previous messages
  * on every interaction with the language model.
- * {@link ChatMemory} helps with keeping track of the conversation and ensuring that messages fit within language model's context window.
+ * [ChatMemory] helps with keeping track of the conversation and ensuring that messages fit within language model's context window.
  */
-public interface ChatMemory {
-
+interface ChatMemory {
     /**
-     * The ID of the {@link ChatMemory}.
-     * @return The ID of the {@link ChatMemory}.
+     * The ID of the [ChatMemory].
+     * @return The ID of the [ChatMemory].
      */
-    Object id();
+    fun id(): Any?
 
     /**
      * Adds a message to the chat memory.
      *
-     * @param message The {@link ChatMessage} to add.
+     * @param message The [ChatMessage] to add.
      */
-    void add(ChatMessage message);
+    fun add(message: ChatMessage?)
 
     /**
      * Retrieves messages from the chat memory.
      * Depending on the implementation, it may not return all previously added messages,
      * but rather a subset, a summary, or a combination thereof.
      *
-     * @return A list of {@link ChatMessage} objects that represent the current state of the chat memory.
+     * @return A list of [ChatMessage] objects that represent the current state of the chat memory.
      */
-    List<ChatMessage> messages();
+    fun messages(): List<ChatMessage?>?
 
     /**
      * Clears the chat memory.
      */
-    void clear();
+    fun clear()
 }
