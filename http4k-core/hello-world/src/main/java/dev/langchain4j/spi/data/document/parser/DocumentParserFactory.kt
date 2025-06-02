c82518fe9ccfef5c01b08f09da92204209f3289e
David Denton <denton.david@gmail.com>
2025-06-02 17:30:58 +0100

    Fix example for graphql

--- http4k-core/hello-world/src/main/java/dev/langchain4j/spi/data/document/parser/DocumentParserFactory.kt
+++ http4k-core/hello-world/src/main/java/dev/langchain4j/spi/data/document/parser/DocumentParserFactory.kt
@@ -1,15 +1,14 @@
-package dev.langchain4j.spi.data.document.parser;
+package dev.langchain4j.spi.data.document.parser
 
-import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentParser
 
 /**
- * A factory for creating {@link DocumentParser} instances through SPI.
- * <br>
- * Available implementations: {@code ApacheTikaDocumentParserFactory}
- * in the {@code langchain4j-document-parser-apache-tika} module.
- * For the "Easy RAG", import {@code langchain4j-easy-rag} module.
+ * A factory for creating [DocumentParser] instances through SPI.
+ * <br></br>
+ * Available implementations: `ApacheTikaDocumentParserFactory`
+ * in the `langchain4j-document-parser-apache-tika` module.
+ * For the "Easy RAG", import `langchain4j-easy-rag` module.
  */
-public interface DocumentParserFactory {
-
-    DocumentParser create();
+interface DocumentParserFactory {
+    fun create(): DocumentParser?
 }
