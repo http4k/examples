6d47d025e311d80775dd95623d709f02a705d79d
David Denton <denton.david@gmail.com>
2025-06-17 22:09:55 +0100

    Add testing libraries

--- http4k-pro/mcp/fully-featured-server/src/test/kotlin/env/RunnableEnvironment.kt
+++ http4k-pro/mcp/fully-featured-server/src/test/kotlin/env/RunnableEnvironment.kt
@@ -3,9 +3,18 @@ package env
 import env.entrylogger.FakeEntryLogger
 import env.oauthserver.TestingOAuthServer
 import env.userdirectory.FakeUserDirectory
+import org.http4k.ai.mcp.client.DiscoveredMcpOAuth
+import org.http4k.ai.mcp.client.http.HttpStreamingMcpClient
+import org.http4k.ai.mcp.model.McpEntity
+import org.http4k.ai.mcp.protocol.Version
+import org.http4k.client.JavaHttpClient
 import org.http4k.config.Environment
+import org.http4k.core.BodyMode.Stream
 import org.http4k.core.Credentials
 import org.http4k.core.Uri
+import org.http4k.core.then
+import org.http4k.filter.ClientFilters
+import org.http4k.filter.debug
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import verysecuresystems.EmailAddress
@@ -35,16 +44,33 @@ fun main() {
 
     val clock = Clock.systemUTC()
 
+    val credentials = Credentials("secureClient", "secret")
+
     TestingOAuthServer(
-        Credentials("securityServer", "securityServerSecret"),
+        credentials,
         authServerUri = Uri.of("http://localhost:${oauthServerPort}"), clock
     ).asServer(SunHttp(oauthServerPort)).start()
 
-    val env = Environment.defaults(
-        PORT of securityServerPort,
-        USER_DIRECTORY_URL of Uri.of("http://localhost:$userDirectoryPort"),
-        ENTRY_LOGGER_URL of Uri.of("http://localhost:$entryLoggerPort"),
-        OAUTH_SERVER_URL of Uri.of("http://localhost:$oauthServerPort")
-    )
-    SecurityMcpServer(env).start()
+    SecurityMcpServer(
+        Environment.defaults(
+            PORT of securityServerPort,
+            USER_DIRECTORY_URL of Uri.of("http://localhost:$userDirectoryPort"),
+            ENTRY_LOGGER_URL of Uri.of("http://localhost:$entryLoggerPort"),
+            OAUTH_SERVER_URL of Uri.of("http://localhost:$oauthServerPort")
+        )
+    ).start()
+
+    val http = ClientFilters.DiscoveredMcpOAuth(credentials)
+        .then(JavaHttpClient(responseBodyMode = Stream).debug())
+
+    HttpStreamingMcpClient(
+        McpEntity.of("foo"),
+        Version.of("1.0.0"),
+        Uri.of("http://localhost:$securityServerPort/mcp"),
+        http
+    ).apply {
+//        println(">>> Server handshake\n" + start())
+        println(">>> Tool list\n" + tools().list())
+        println(">>> Prompt list\n" + prompts().list())
+    }
 }
